30f663d09bc89a6afb460a704c04460e2199f0e5
Juan Carlos Delgado <jcarloscondor@gmail.com>
2014-10-31 17:15:51 +0100

    la he liao parda

--- modules/xmas/public/javascripts/controllers/highlight_controller.js
+++ modules/xmas/public/javascripts/controllers/highlight_controller.js
@@ -15,7 +15,49 @@ angular.module('IntrepidJS').controller('CtrlHighlight',
         '$scope',
         '$state',
         function ($scope, $state) {
-            $scope.titulo = "Titulo"
+            $scope.titulo = "Titulo";
+
+            $scope.categories = [
+              {
+                title: 'De 0 a 3 años',
+                items: [
+                  {
+                    _id: 1,
+                    url: 'http://fotos.eluniversal.com.mx/web_img/fotogaleria/kate1.jpg',
+                    vote: true
+                  }
+                ]
+              },
+              {
+                title: 'De 3 a 6 años',
+                items: [
+                  {
+                    _id: 2,
+                    url: 'http://g.cdn.ecn.cl/fotografia/files/2014/06/fotos-perturbadoras-6.jpg',
+                    vote: false
+                  }
+                ]
+              }
+            ]
+
+            getXmas = function() {
+              $http.get("/api/xmas")
+              .success(function(data) {
+
+               var categories = _.map(data, function(d){
+                 return d.category
+               });
+
+               var uniq_categories = _.uniq(categories);
+
+               data.forEach(function(d) {
+
+               })
+
+             });
+
+            }
+
         }
     ]
 );
