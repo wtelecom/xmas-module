fd22689c94857e808a2134cae6ddd12ebdc353b8
Javimudi <javier.munoz.diaz@gmail.com>
2014-10-31 18:33:06 +0100

    LDAP auth almost working

--- routes/accounts/index.js
+++ routes/accounts/index.js
@@ -3,8 +3,15 @@
  */
 
 var passport = require("passport");
+var LdapStrategy = require("passport-ldapauth").Strategy;
 var rek = require('rekuire');
+var settings = rek('/settings');
+var Account = rek('data/models/user/account');
 var signupPassport = rek('middlewares/users/signup_user');
+
+
+passport.use(new LdapStrategy(settings.ldapSettings));
+
 
 module.exports = {
     '/accounts/signup': [
@@ -32,13 +39,45 @@ module.exports = {
         {
             methods: ['post'],
             fn: function(req, res, next) {
-                passport.authenticate('local', function(err, user, info) {
+                passport.authenticate('ldapauth', function(err, user, info) {
                     if (err) { return next(err); }
                     if (!user) { return res.send({success: false}); }
-                    req.logIn(user, function(err) {
-                        if (err) { return next(err); }
-                        return res.send({success: true});
+                    
+                    var uid = { 'username': user.sAMAccountName};
+                    
+                    var local_user = null;
+                    
+                    
+                    Account.findOne( uid, uid, function(error, nRows, result){
+                        console.log(result);
+                        
+                        if (error) { // Create
+                            Account.insert(uid, uid, function(error, nRows, result){
+                                
+                                
+                                console.log(error);
+                                console.log(nRows);
+                                console.log(result);
+                                
+                                
+                                if (!error) { local_user = result; }
+                                else { return next(err); }
+                                
+                            });
+                        }
+                        
+                        console.log(result);
+                        local_user = result;
+                        console.log(local_user);
+                        
+                        
+                        req.logIn(local_user, function(err) {
+                                if (err) { return next(err); }
+                                return res.send({success: true});
+                        });
                     });
+                    
+     
                 })(req, res, next);
             }
         }
