ab0edd432c5e97aa6ef552f5a2297c0203823a5f
Sergio Soto Nuñez <ssoto@wtelecom.es>
2014-10-31 18:56:43 +0100

    Added function to Xmas and Config models. IT ISN'T TESTED, COULD FAIL (SURE)

--- modules/xmas/data/schemas/xmas.js
+++ modules/xmas/data/schemas/xmas.js
@@ -8,18 +8,23 @@ var mongoose = require('mongoose'),
     _ = require('underscore');
 
 var xmasSchema = new mongoose.Schema({
-	author: {
-		type: mongoose.Schema.Types.ObjectId,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Account'
-	},
-	votes: [{ type: mongoose.Schema.Types.ObjectId,
-			 ref: 'Account'}],
-	uploaded_at: {type: Date},
-	artist: String,
-	category: {type: String},
-	url: String
+    },
+    votes: [{ type: mongoose.Schema.Types.ObjectId,
+             ref: 'Account'}],
+    uploaded_at: {type: Date},
+    artist: String,
+    category: {type: String},
+    url: String
 });
 
+
+xmasSchema.statics.getImages = function(req, next){
+    var current_step = configSchema.getCurrentStep();
+    
+}
 
 module.exports = xmasSchema;
 
