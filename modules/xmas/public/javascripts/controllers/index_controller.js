46a447ae1a143d17321db01da1d2badc821c0ae0
Juan Carlos Delgado <jcarloscondor@gmail.com>
2014-10-31 17:11:57 +0100

    dummy data

--- modules/xmas/public/javascripts/controllers/index_controller.js
+++ modules/xmas/public/javascripts/controllers/index_controller.js
@@ -14,8 +14,47 @@ angular.module('IntrepidJS').controller('XmasIndexController',
     [
         '$scope',
         '$state',
-        function ($scope, $state) {
+        '$http',
+        function ($scope, $state, $http) {
             console.log("djsfjdshgfhdsghf");
+
+            $scope.categories = [
+              {
+                title: 'De 0 a 3 años',
+                items: [
+                  {
+                    _id: 1,
+                    url: 'http://fotos.eluniversal.com.mx/web_img/fotogaleria/kate1.jpg',
+                    vote: true
+                  }
+                ]
+              },
+              {
+                title: 'De 3 a 6 años',
+                items: [
+                  {
+                    _id: 2,
+                    url: 'http://g.cdn.ecn.cl/fotografia/files/2014/06/fotos-perturbadoras-6.jpg',
+                    vote: false
+                  }
+                ]
+              }
+            ]
+
+
+            getXmas = function() {
+              $http.get("/api/xmas")
+              .success(function(data) {
+               categories = _.map(data, function(d){
+                 return d.category
+               });
+
+               uniq_categories = _.uniq(categories);
+
+             });
+
+            }
+
         }
     ]
 );
