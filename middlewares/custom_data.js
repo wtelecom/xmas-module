d0e7a033b57208c868b28bfe9f848d663b5deaec
Oscar Ramirez <tuxskar@gmail.com>
2014-11-15 15:07:25 +0100

    Updated header.jade in order to have signup tab on local authentication configuration

--- middlewares/custom_data.js
+++ middlewares/custom_data.js
@@ -4,10 +4,14 @@
  * @desc This module loads csutom data available in views
  */
 
+var rek = require('rekuire'),
+    settings = rek('../settings');
+
 module.exports = function(app) {
     app.use(function(req, res, next) {
         // Setting user info
         res.locals.user = req.user;
+        res.locals.auth = settings.auth_method;
 
         // Setting underscore
         res.locals._ = require("underscore");
